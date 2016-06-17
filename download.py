73526a4b67d611ae2a076c79d77e997b512b828a
Chinseok Lee <allieuslee@gmail.com>
2016-06-18 02:58:56 +0900

    Azure WebApp 다운로드 스크립트 추가

---
+++ download.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import requests
+
+
+def main():
+    base = 'https://raw.githubusercontent.com/askdjango/azure-webapp-django-setup/master/'
+    names = ['.deployment', 'deploy.py', 'deploy_settings.py', 'web.3.4.config', 'ptvs_virtualenv_proxy.py']
+
+    for name in names:
+        url = base + name
+        print('downloading {} ...'.format(name))
+        open(name, 'wb').write(requests.get(url).content)
+
+
+if __name__ == '__main__':
+    main()
+
